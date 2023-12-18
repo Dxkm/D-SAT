3d00b39020d50d6df08ba393f88faa683533f078
Km <69192854+Dxkm@users.noreply.github.com>
2023-12-18 14:08:22 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,79 @@
+import os
+import sys
+import yaml
+import torch
+import logging
+import argparse
+from thop import profile
+from torch.utils.data import DataLoader
+
+from models.TCUNet import TCUNet
+from utils.trainer_npy import inf
+from configs import cswin_config as cfg
+from data.dataset_ACDC import ACDCDataset
+from data.dataset_synapse import SynapseDataset
+from utils.utils import accuracy, inference, random_seed, count_params
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--item_name', default='ACDC', help='The item name')
+    args = parser.parse_args()
+    return args
+
+
+def Synapse_test_loader(config):
+    db_test = SynapseDataset(base_dir=config['test_path'], split="test_vol", list_dir=config['list_dir'])
+    test_loader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=config['num_workers'])
+    return test_loader
+
+
+def acdc_test_loader(config):
+    db_test = ACDCDataset(config['ac_test_dir'], config['ac_list_dir'], split="test")
+    test_loader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=config['num_workers'])
+    return test_loader
+
+
+def main():
+    args = parse_args()
+    with open('results/%s/config.yml' % args.item_name, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    random_seed(config['seed'], state=True)
+
+    models_name = {
+        'tcu-t': cfg.TCU_T(),
+        'tcu-s': cfg.TCU_S(),
+    }
+    model = TCUNet(cfg=models_name[config['arch']], num_classes=config['num_classes'],
+                   image_size=config['input_w']).cuda()
+
+    # G FLOPs and parameter
+    put = torch.zeros((1, 3, 224, 224)).cuda()
+    flops, _ = profile(model, inputs=(put,))
+    print("G FLOPs：", flops / 1e9)
+    count_params(model)
+
+    if config['dataset'] == 'Synapse' or config['dataset'] == 'ACDC':
+        os.makedirs(os.path.join(config['output_dir'], config['item_name'], 'test'), exist_ok=True)
+        test_save_path = os.path.join(config['output_dir'], config['item_name'], 'test')
+        logging.basicConfig(
+            filename=config['output_dir'] + '/' + config['item_name'] + '/' + "text.txt",
+            level=logging.INFO, format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+        logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+        if config['dataset'] == 'Synapse':
+            model.load_state_dict(torch.load('results/%s/epoch_249.pth' % config['item_name']), strict=False)
+            inference(config, model, test_loader=Synapse_test_loader(config), test_save_path=test_save_path)
+        elif config['dataset'] == 'ACDC':
+            model.load_state_dict(torch.load('results/%s/model.pth' % config['item_name']), strict=False)
+            inference(config, model, test_loader=acdc_test_loader(config), test_save_path=test_save_path)
+    elif config['dataset'] == 'ISIC2018':
+        model.load_state_dict(torch.load('results/%s/model.pth' % config['item_name']), strict=False)
+        inf(config, model)
+    else:
+        model.load_state_dict(torch.load('results/%s/model.pth' % config['item_name']), strict=False)
+        accuracy(config, model)
+
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,137 @@
+import os
+import yaml
+import torch
+import argparse
+from torch.optim import lr_scheduler as lr
+
+from models.TCUNet import TCUNet
+from utils.trainer import trainer
+from utils.losses import BCEDiceLoss
+from configs import cswin_config as cfg
+from utils.trainer_npy import trainer_npy
+from utils.trainer_acdc import trainer_acdc
+from utils.trainer_Synapse import trainer_synapse
+from utils.utils import count_params, str2bool, random_seed
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--item_name', default='test_acdc', help='The item name')
+
+    # dataset
+    parser.add_argument('--dataset', type=str, default='ACDC',
+                        choices=['ISIC2018', 'CVC-ClinicDB', 'Kvasir-SEG', 'Synapse', 'ACDC'], help='dataset')
+    parser.add_argument('--num_classes', type=int, default=1, help='Number of classes')
+    parser.add_argument('--output_dir', type=str, default='./results', help='root dir for output log')
+
+    # Synapse dataset
+    parser.add_argument('--root_path', type=str, default='../Data/Synapse/train_npz', help='root dir for data')
+    parser.add_argument('--test_path', type=str, default='../Data/Synapse/test_vol_h5', help='root dir for data')
+    parser.add_argument('--list_dir', type=str, default='../Data/Synapse/lists/lists_Synapse', help='list dir')
+
+    # ACDC dataset
+    parser.add_argument('--ac_list_dir', type=str, default='../Data/ACDC/lists_ACDC')
+    parser.add_argument('--ac_test_dir', type=str, default='../Data/ACDC/test')
+
+    # others dataset
+    parser.add_argument('--root_dir', default='../Data', help='Dataset root dir')
+    parser.add_argument('--img_ext', default='.jpg', help='image file extension')
+    parser.add_argument('--mask_ext', default='.jpg', help='mask file extension')
+
+    # Network
+    parser.add_argument('--arch', type=str, default='tcu-t', choices=['tcu-t', 'tcu-s'], help='Net')
+    parser.add_argument('--in_channels', type=int, default=3, help='Input channels')
+    parser.add_argument('--input_w', type=int, default=224, help='image width')
+    parser.add_argument('--input_h', type=int, default=224, help='image height')
+    parser.add_argument('--epochs', type=int, default=200, help='Number of total epochs to run')
+    parser.add_argument('--batch_size', type=int, default=4, help='Mini-batch size (default: 8)')
+    parser.add_argument('--save_interval', type=int, default=5, help='evaluation epoch')
+
+    # optimizer and scheduler
+    parser.add_argument('--lr_scheduler', type=str, default='Cosine', choices=['Cosine', 'Reduce'], help='lr_scheduler')
+    parser.add_argument('--lr', type=float, default=1e-4, help='Initial learning rate')
+    parser.add_argument('--min_lr', type=float, default=1e-6, help='Minimum learning rate')
+
+    # Others
+    parser.add_argument('--seed', type=int, default=1234, help='Random seed')
+    parser.add_argument('--num_workers', type=int, default=0, help='Number of workers')
+    parser.add_argument('--deep_supervision', type=str2bool, default=False, help='Deep supervision')
+    parser.add_argument('--early_stopping', type=int, default=50, help='Early stopping (default: 50)')
+
+    config = parser.parse_args()
+    return config
+
+
+def main():
+    config = vars(parse_args())
+
+    # Dataset choose
+    config['root_dir'] = config['root_dir'] + '/' + config['dataset']
+    if config['dataset'] == 'Kvasir-SEG':
+        config['img_ext'] = '.jpg'
+        config['mask_ext'] = '.jpg'
+        config['lr_scheduler'] = 'Reduce'
+    elif config['dataset'] == 'CVC-ClinicDB':
+        config['img_ext'] = '.tif'
+        config['mask_ext'] = '.tif'
+    elif config['dataset'] == 'ISIC2018':
+        config['img_ext'] = '.jpg'
+        config['mask_ext'] = '.png'
+        config['num_classes'] = 2
+        config['lr_scheduler'] = 'Reduce'
+    elif config['dataset'] == 'Synapse':
+        config['num_classes'] = 9
+        config['epochs'] = 250
+    elif config['dataset'] == 'ACDC':
+        config['num_classes'] = 4
+        config['epochs'] = 100
+        config['lr_scheduler'] = 'Reduce'
+
+    # Sava config as config.yaml
+    os.makedirs('results/%s' % config['item_name'], exist_ok=True)
+    with open('results/%s/config.yml' % config['item_name'], 'w') as f:
+        yaml.dump(config, f)
+
+    # Set Random Seed
+    random_seed(config['seed'], state=True)
+
+    # Set Network  D-SAT
+    models_name = {
+        'tcu-t': cfg.TCU_T(),
+        'tcu-s': cfg.TCU_S(),
+    }
+    model = TCUNet(cfg=models_name[config['arch']], num_classes=config['num_classes'],
+                   image_size=config['input_w']).cuda()
+
+    count_params(model)
+
+    # Set loss function (criterion)
+    criterion = BCEDiceLoss()
+
+    # Optimization Function and lr_scheduler
+    params = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = torch.optim.AdamW(params, lr=config['lr'])
+
+    if config['lr_scheduler'] == 'Cosine':
+        scheduler = lr.CosineAnnealingLR(optimizer, T_max=config['epochs'], eta_min=config['min_lr'])
+        # Start training
+        if config['dataset'] == 'Synapse':
+            trainer_synapse(config, model, optimizer, scheduler)
+        elif config['dataset'] == 'ACDC':
+            trainer_acdc(config, model, optimizer, scheduler)
+        else:
+            trainer(config, model, optimizer, scheduler, criterion)
+    elif config['lr_scheduler'] == 'Reduce':
+        scheduler = lr.ReduceLROnPlateau(optimizer, mode="max", factor=0.5, min_lr=config['min_lr'], verbose=True)
+        # Start training
+        if config['dataset'] == 'ISIC2018':
+            trainer_npy(config, model, optimizer, scheduler, criterion)
+        elif config['dataset'] == 'ACDC':
+            trainer_acdc(config, model, optimizer, scheduler)
+        else:
+            trainer(config, model, optimizer, scheduler, criterion)
+
+
+if __name__ == '__main__':
+    main()
